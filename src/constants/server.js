a988f9be2e380fa98f98b91097ee616a3bbad781
Administrator <adriandlai@gmail.com>
2018-06-20 15:03:15 -0600

    updated submiteREADme for urls

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,5 +1,4 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  // ? 'https://bucket3proj.firebaseapp.com'
   ? 'https://b3projbackend.herokuapp.com/stripe'
   : 'http://localhost:3000/stripe';
 
--- submitREADME.md
+++ submitREADME.md
@@ -1,2 +1,2 @@
 backend github url https://github.com/adriandlai/bucket3proj-server
-depoloyed FE url 
+depoloyed FE url https://bucket3proj.firebaseapp.com
