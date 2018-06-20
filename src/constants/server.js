992f95ede2a18a1f3cbc1af6d97b4c4280996f26
Administrator <adriandlai@gmail.com>
2018-06-20 14:54:16 -0600

    updated stripe backend URL

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,5 +1,6 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'https://bucket3proj.firebaseapp.com'
+  // ? 'https://bucket3proj.firebaseapp.com'
+  ? 'https://b3projbackend.herokuapp.com/stripe'
   : 'http://localhost:3000/stripe';
 
 export default PAYMENT_SERVER_URL;
